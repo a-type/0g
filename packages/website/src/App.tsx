3ad88fade8e4581c630d88df071565dcedd627a9
Grant Forrest <gaforres@gmail.com>
2020-12-10 15:04:53 -0500

    add public url to router

--- packages/website/src/App.tsx
+++ packages/website/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route } from 'wouter';
+import { Route, Router } from 'wouter';
 import { Header } from './components/Header';
 import { HomePage } from './pages/HomePage';
 import { ThemeProvider } from '@emotion/react';
@@ -7,14 +7,16 @@ import { theme } from './theme';
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <div className="App">
-        <Header />
-        <Route path="/">
-          <HomePage />
-        </Route>
-      </div>
-    </ThemeProvider>
+    <Router base={process.env.PUBLIC_URL}>
+      <ThemeProvider theme={theme}>
+        <div className="App">
+          <Header />
+          <Route path="/">
+            <HomePage />
+          </Route>
+        </div>
+      </ThemeProvider>
+    </Router>
   );
 }
 
